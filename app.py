0990f5dacea92a7721e3ed70faf3cd16d2385706
Theriley106 <christopherlambert106@gmail.com>
2018-03-22 17:41:46 -0400

    Added interactions as a dependancy in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response, jsonify
-
+import interactions
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 
