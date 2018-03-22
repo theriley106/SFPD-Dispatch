91370d3d25b923c9dadeda02a35100985e3cd159
Theriley106 <christopherlambert106@gmail.com>
2018-03-22 17:19:57 -0400

    Added initial app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response, jsonify
+
+
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+
+@app.route('/', methods=['GET'])
+def index():
+	return "<h1>This Works</h1>"
+
+if __name__ == "__main__":
+	app.run()
