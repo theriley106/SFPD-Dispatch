6808c579ecba9a108595cd695061bd78e639fb03
Theriley106 <christopherlambert106@gmail.com>
2018-03-22 17:41:13 -0400

    Added interactions to convert the csv to a list of dictionaries

--- interactions.py
+++ interactions.py
@@ -4,8 +4,25 @@ DATASET_FILE = "datasets/sfpd_dispatch_data_subset.csv"
 
 def readDataset():
 	# This will convert the dataset into a format the the front end will use
-	for val in csvToList(DATASET_FILE):
-		# Converts the SFPD dataset into a list
+	dataset = []
+	# Ideally this should be  a list of JSON/python dicts for the jinja template
+	datasetList = csvToList(DATASET_FILE)
+	# This is the dataset in list format
+	datasetSchema = datasetList[0]
+	# "call_number", "unit_id", etc.
+	datasetList = datasetList[1:]
+	# This removes the top row, which indicates the row values | ie: not actual data
+	for val in datasetList:
+		tempInfo = {}
+		# This is overwritten on each loop
+		for i in range(len(datasetSchema)):
+			# Coverts each line in the CSV into JSON
+			tempInfo[datasetSchema[i]] = val[i]
+			# ie: dataset['call_number'] = 12312312321
+		dataset.append(tempInfo)
+		# Adds the python dict to the dataset array
+	return dataset
+	# Returns an array of python dictionaries
 
 
 def csvToList(csvFile):
@@ -13,3 +30,4 @@ def csvToList(csvFile):
 	with open(csvFile, 'rb') as f:
 		reader = csv.reader(f)
 		return list(reader)
+
