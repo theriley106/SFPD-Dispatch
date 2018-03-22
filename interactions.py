fad1156f99a43cb1062d7e1070c36f37a53acc93
Theriley106 <christopherlambert106@gmail.com>
2018-03-22 17:30:22 -0400

    Fixed the weird tabs->space error

--- interactions.py
+++ interactions.py
@@ -4,10 +4,12 @@ DATASET_FILE = "datasets/sfpd_dispatch_data_subset.csv"
 
 def readDataset():
 	# This will convert the dataset into a format the the front end will use
-	csvToList(DATASET_FILE)
+	for val in csvToList(DATASET_FILE):
+		# Converts the SFPD dataset into a list
+
 
 def csvToList(csvFile):
 	# This will return a list of rows in the csv file
 	with open(csvFile, 'rb') as f:
-	    reader = csv.reader(f)
-	    your_list = list(reader)
+		reader = csv.reader(f)
+		return list(reader)
