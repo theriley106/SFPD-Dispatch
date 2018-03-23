60717536060073f281a4c6a57241566e7c8a4036
Theriley106 <christopherlambert106@gmail.com>
2018-03-23 13:21:59 -0400

    Added findAmountOfUniqueLocations()

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -7,3 +7,10 @@ This is a not production ready code, but I wanted to include it anyway.
 This is the code used to generate the data visualizations and analytics
 in the readme
 '''
+def findAmountOfUniqueLocations():
+	a = returnListOfParam(incidentList, param, duplicates=True)
+	b = returnListOfParam(incidentList, param, duplicates=False)
+	return (float(b) / float(a)) * 100
+
+def findMostFrequentLocation():
+	incidentList = readDataset()
