5c89cc6873bb3701acddfa8fab898eda81130b9d
Theriley106 <christopherlambert106@gmail.com>
2018-03-23 08:48:36 -0400

    Added MeanHousehold to readDataset

--- interactions.py
+++ interactions.py
@@ -79,7 +79,11 @@ def readDataset():
 			tempInfo[datasetSchema[i]] = val[i]
 			# ie: dataset['call_number'] = 12312312321
 		if tempInfo["priority"] == "E":
+			# This messes up the frontend when priority is not an int
 			tempInfo["priority"] = 4
+			# Sets it to 4, which is basically "Unknown"
+		tempInfo["MeanHousehold"] = returnHousholdIncome(tempInfo["zipcode_of_incident"])
+		# Ideally this would be a value IN THE dataset, but I don't want to modify the dataset from mindsumo
 		tempInfo["mapColor"] = getPriorityColor(tempInfo["priority"])
 		dataset.append(tempInfo)
 		# Adds the python dict to the dataset array
